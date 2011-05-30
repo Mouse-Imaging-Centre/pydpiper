71f413cd0e457deb8becf0929bccd1db744038f0
Jason Lerch <jason@topogigio.phenogenomics.ca>
2011-05-30 14:43:47 -0400

    * updated pipeline-tests to be class * references #11

--- pipeline-tests.py
+++ pipeline-tests.py
@@ -1,16 +1,29 @@
 #!/usr/bin/env python
 
 from pipeline import *
+import networkx as nx
 
 def generateFile(i):
     return("filename_" + str(i) + ".mnc")
 
-def test_simple_pipeline():
-    p = Pipeline()
-    startFile = generateFile(0)
-    p.addStage(CmdStage(["somecommand", startFile, generateFile(1)]))
-    for i in range(2,100):
-        p.addStage(CmdStage(["somecommand", generateFile(i-1), generateFile(i)]))
-    p.initialize()
-    runnableStage = p.getRunnableStageIndex()
-    assert runnableStage == 0
+class TestSimplePipeline():
+    def setup_method(self, method):
+        self.p = Pipeline()
+        startFile = generateFile(0)
+        self.p.addStage(CmdStage(["somecommand", InputFile(startFile), OutputFile(generateFile(1))]))
+        for i in range(2,100):
+            self.p.addStage(CmdStage(["somecommand", InputFile(generateFile(i-1)), OutputFile(generateFile(i))]))
+        self.p.initialize()
+        nx.write_dot(self.p.G, "simple-test-pipeline.dot")
+
+    def test_graph_head(self):
+        """make sure that it finds the graph head correctly"""
+        runnableStage = self.p.getRunnableStageIndex()
+        assert runnableStage == 0
+
+    def test_dependency(self):
+        """make sure that random dependency is correct"""
+        s = self.p.G.predecessors(10)
+        print "S: " + str(s)
+        assert s[0] == 9
+        
