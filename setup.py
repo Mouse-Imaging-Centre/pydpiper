7ca6c3e068f36750790c037d12ef65bbbd9a0096
Fraser Macdonald <macdonaldfk@gmail.com>
2011-06-09 14:48:04 -0400

    Modified the check for installed packages, switched from distutil to setuptools, fixes #22.

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env/python
 
-from distutils.core import setup
+from setuptools import setup
 import sys
 
 """ Testing required packages & Python version """
 prepackages_displaylist = {'networkx': 'networkx (http://networkx.lanl.gov)', 'Pyro': 'Pyro (http://irmen.home.xs4all.nl/pyro3/)',
-                           'pytest': 'pytest (http://doc.pytest.org/en/latest/) [OPTIONAL]'}
+                          'pytest': 'pytest (http://doc.pytest.org/en/latest/) [OPTIONAL]'}
 prepackages_stringlist = {'networkx': 'installed', 'Pyro': 'installed', 'pytest': 'installed'}
 
 print 'Testing required packages ...'
 
-printstr = " Python >= v. 2.6"
-print printstr.ljust(60, ' '),
-if sys.version_info<=(2,6,0):
+printstr = "Python >= v. 2.6"
+print ' ' + printstr.ljust(60, ' '),
+if sys.version_info<=(2,5,0):
     print 'not installed (current v. = ' + sys.version[:5] + ')'
     sys.exit("ERROR: Cancelling install. Please update Python to at least version 2.6")
 else:
@@ -23,24 +23,25 @@ for key in prepackages_stringlist.keys():
         __import__(key)
     except ImportError:
         prepackages_stringlist[key] = 'not installed'
-    print ' {0:60}{1:15}'.format(prepackages_displaylist[key], prepackages_stringlist[key])
-
+    print ' ' + prepackages_displaylist[key].ljust(61, ' ') + prepackages_stringlist[key]
 
 print 'Done testing required packages.\n'
 
     
-
-""" Setup Info """
-setup(name='pydpiper',
-      version='0.1',
-      license='Modified BSD',
-      description='Python code for flexible pipeline control',
-      long_description = 'Python code for flexible pipeline control', 
-      author='Jason Lerch, Miriam Friedel, Fraser MacDonald, Matthijs Van Eede',
-      maintainer_email='mfriedel@phenogenomics.ca',
-      url='https://github.com/mfriedel/pydpiper',
-      platforms="any",
-      packages=['pydpiper'], 
-      scripts=['pipeline_executor.py'],
-      requires=['networkx','Pyro']
-      )
+try:
+    setup(name='pydpiper',
+          version='0.1',
+          license='Modified BSD',
+          description='Python code for flexible pipeline control',
+          long_description = 'Python code for flexible pipeline control', 
+          author='Jason Lerch, Miriam Friedel, Fraser MacDonald, Matthijs Van Eede',
+          maintainer_email='mfriedel@phenogenomics.ca',
+          url='https://github.com/mfriedel/pydpiper',
+          platforms="any",
+          packages=['pydpiper'], 
+          scripts=['pipeline_executor.py'],
+          install_requires=['networkx', 'Pyro']
+          )
+except ImportError, e:
+    print e
+    
