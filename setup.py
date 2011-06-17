9f7efefa0679eff7ebdf1a7fa01f26bb9a60c2c0
Miriam Friedel <mfriedel@phenogenomics.ca>
2011-06-17 16:01:35 -0400

    Reverted setup.py and INSTALL to 591395349e921aac2c7 until we get newer changes working properly.

--- setup.py
+++ setup.py
@@ -1,47 +1,16 @@
 #!/usr/bin/env/python
 
-from setuptools import setup
-import sys
+from distutils.core import setup
 
-""" Testing required packages & Python version """
-prepackages_displaylist = {'networkx': 'networkx (http://networkx.lanl.gov)', 'Pyro': 'Pyro (http://irmen.home.xs4all.nl/pyro3/)',
-                          'pytest': 'pytest (http://doc.pytest.org/en/latest/) [OPTIONAL]'}
-prepackages_stringlist = {'networkx': 'installed', 'Pyro': 'installed', 'pytest': 'installed'}
-
-print 'Testing required packages ...'
-
-printstr = "Python >= v. 2.6"
-print ' ' + printstr.ljust(60, ' '),
-if sys.version_info<=(2,5,0):
-    print 'not installed (current v. = ' + sys.version[:5] + ')'
-    sys.exit("ERROR: Cancelling install. Please update Python to at least version 2.6")
-else:
-    print 'installed'
-
-for key in prepackages_stringlist.keys():
-    try:             
-        __import__(key)
-    except ImportError:
-        prepackages_stringlist[key] = 'not installed'
-    print ' ' + prepackages_displaylist[key].ljust(61, ' ') + prepackages_stringlist[key]
-
-print 'Done testing required packages.\n'
-
-    
-try:
-    setup(name='pydpiper',
-          version='0.1',
-          license='Modified BSD',
-          description='Python code for flexible pipeline control',
-          long_description = 'Python code for flexible pipeline control', 
-          author='Jason Lerch, Miriam Friedel, Fraser MacDonald, Matthijs Van Eede',
-          maintainer_email='mfriedel@phenogenomics.ca',
-          url='https://github.com/mfriedel/pydpiper',
-          platforms="any",
-          packages=['pydpiper'], 
-          scripts=['pipeline_executor.py'],
-          install_requires=['networkx', 'Pyro']
-          )
-except ImportError, e:
-    print e
-    
+setup(name='pydpiper',
+      version='0.1',
+      license='Modified BSD',
+      description='Python code for flexible pipeline control',
+      long_description = 'Python code for flexible pipeline control', 
+      author='Jason Lerch, Miriam Friedel, Fraser MacDonald, Matthijs Van Eede',
+      maintainer_email='mfriedel@phenogenomics.ca',
+      url='https://github.com/mfriedel/pydpiper',
+      platforms="any",
+      packages=['pydpiper'], 
+      scripts=['pipeline_executor.py']
+      )
