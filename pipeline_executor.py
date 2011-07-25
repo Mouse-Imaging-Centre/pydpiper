5ea7db439696939ecf1a24344dff7728056495e1
Fraser Macdonald <macdonaldfk@gmail.com>
2011-07-25 17:08:03 -0400

    Pipeline executor now supports multiple processes

--- pipeline_executor.py
+++ pipeline_executor.py
@@ -23,37 +23,35 @@ class Executor(Pyro.core.SynchronizedObjBase):
             self.continueRunning = False
          
 
-def executePipeline(serverURI, executor):
-    # Need to get own proxy here, since separate thread
+def runStage(serverURI, s):
+    # Proc needs its own proxy as it's independent of executor
     p = Pyro.core.getProxyForURI(serverURI)
-    #while True:
-    while executor.continueLoop:
-        try:
-            i = p.getRunnableStageIndex()
-            if i == None:
-            	print("No runnable stages. Going to sleep")
-            	time.sleep(5)
-            else:
-            	s = p.getStage(i)
-            	print("Running stage " + str(i) + ":")
-            	print(s)
-            	r = s.execStage()
-            	print("return was: " + str(r))
-            	if r == 0:
-                    p.setStageFinished(i)
-            	else:
-                    p.setStageFailed(i)
-                ps = p.getProcessedStageCount()
-                print("Number of processed stages: " + str(ps) + "\n")
-        except:
-            print "Failed in executor thread"
-    	    print "Unexpected error: ", sys.exc_info()
-    	    sys.exit()
+
+    # Run stage, set finished or failed accordingly  
+    try:
+        print("Running stage " + str(i) + ":")
+        print(s),
+        print "\n"
+        r = s.execStage()
+        print("Stage " + str(i) + " finished, return was: " + str(r) + "\n")
+        if r == 0:
+            p.setStageFinished(i)
+        else:
+            p.setStageFailed(i)
+    except:
+        print "Failed in executor thread"
+        print "Unexpected error: ", sys.exc_info()
+    	sys.exit()        
+
+
+##########     ---     Start of program     ---     ##########
+    
 
 if __name__ == "__main__":
     usage = "%prog [options]"
     description = "pipeline executor"
-    
+
+    # command line option handling    
     parser = OptionParser(usage=usage, description=description)
     
     parser.add_option("-u", "--uri-file", dest="urifile",
@@ -64,12 +62,13 @@ if __name__ == "__main__":
                       help="Use the Pyro NameServer to store object locations")
                       
     (options,args) = parser.parse_args()
-       
+
+    # initialize pipeline_executor as both client and server       
     Pyro.core.initClient()
     Pyro.core.initServer()
     daemon = Pyro.core.Daemon()
     
-    #initialize executor as both client and server
+    # set up communication with server from the URI string
     if options.use_ns:
         ns = Pyro.naming.NameServerLocator().getNS()
         serverURI = ns.resolve("pipeline")
@@ -86,29 +85,39 @@ if __name__ == "__main__":
     	except IOError:
     	    print "IOError: No uri file in working directory.  Specify uri file location with '-u'."
     	    sys.exit()
-    
+
+    # instantiate the executor class and register the executor with the pipeline    
     executor = Executor()
     clientURI=daemon.connect(executor,"executor")
     p = Pyro.core.getProxyForURI(serverURI)
     p.register(clientURI)
-    
-    process = Process(target=executePipeline, args=(serverURI, executor))
-    process.start()
+      
+    # loop until the pipeline sets continueRunning to false
     try:
-        daemon.requestLoop(executor.continueLoop)
+        while executor.continueLoop():
+            daemon.handleRequests(0)  #functions in place of daemon.requestLoop() to allow for custom event loop
+            try:
+                i = p.getRunnableStageIndex()
+                if i == None:
+                    print("Sleeping ...")
+                    time.sleep(5)
+                    # This is still buggy - shorter sleep times cause the executor to miss
+                    # the ShutdownCall and crash when calling p.getRunnableStageIndex()
+                else:
+                    s = p.getStage(i)
+                    process = Process(target=runStage, args=(serverURI, s))
+                    process.start()
+            except:
+                print "Executor failed"
+                print "Unexpected error: ", sys.exc_info()
+                sys.exit()
+                
     except:
         print "Failed in pipeline_executor"
-    	print "Unexpected error: ", sys.exc_info()
-    	process.join()
-    	daemon.shutdown(True)
-    	sys.exit()
+        print "Unexpected error: ", sys.exc_info()
+        daemon.shutdown(True)
+        sys.exit()
     else:
         print "Server has shutdown. Killing executor thread."
         process.terminate()
         daemon.shutdown(True)
-    
-    
-    
-
-    
-        
