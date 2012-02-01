e2e6a6374a32d1c02705fb14fe4ad8c58d9d9ea1
Jon Pipitone <jon.pipitone@gmail.com>
2012-02-01 07:23:43 -0500

    Extracted the MAGeT algorithm into the class BasicMAGeT.  Untested.

--- applications/MAGeT_human/MAGeT.py
+++ applications/MAGeT_human/MAGeT.py
@@ -47,13 +47,13 @@ class Template:
         self.directory = directory   
 
 
-class SMATregister:
+class BasicLabelPropogationStrategy:
     def __init__(self, target, template, root_dir=""):
-        """Register input image (target) to a labelled template (template).
+        """Register input image (target) to a labelled template (template) and propogate its labels.
         
             root_dir is the directory under which to place the output directories and files.
-            The directory structure produced is <root_dir>/<template>/<target>/. All registration files are placed
-            in that folder  
+            The directory structure produced is <root_dir>/<template>/<target>/. 
+            All intermediate registration files and labels are placed in that folder  
         """
         
         
@@ -62,6 +62,39 @@ class SMATregister:
         self.root_dir = root_dir
         
     
+
+    def do_linear_registration(self, p, log_base, output_base_fname, inuc):
+        linxfm, linreg_log = fh.createOutputAndLogFiles(output_base_fname, log_base, "lin.xfm")
+        p.addStage(bestlinreg(inuc, self.template.image, linxfm, linreg_log))
+        return linxfm
+
+
+    def do_non_linear_registration(self, p, log_base, output_base_fname, inuc, linxfm):
+        linres, linres_log = fh.createOutputAndLogFiles(output_base_fname, log_base, "linres.mnc")
+        p.addStage(mincresample(inuc, linres, linres_log, argarray=["-sinc", "-width", "2"], like=self.template.image, cxfm=linxfm))
+    # non-linear registration
+        iterations = 15
+        nl0, logfile0 = fh.createOutputAndLogFiles(output_base_fname, log_base, "nl_0.xfm")
+        nl1, logfile1 = fh.createOutputAndLogFiles(output_base_fname, log_base, "nl_1.xfm")
+        nl2, logfile2 = fh.createOutputAndLogFiles(output_base_fname, log_base, "nl_2.xfm")
+        p.addStage(minctracc(linres, self.template.roi, nl0, logfile0, step=4, sub_lattice=8, iterations=iterations, ident=True))
+        p.addStage(minctracc(linres, self.template.roi, nl1, logfile1, step=2, sub_lattice=8, transform=nl0, iterations=iterations, ident=True))
+        p.addStage(minctracc(linres, self.template.roi, nl2, logfile2, step=1, sub_lattice=6, transform=nl1, iterations=iterations))
+        nlxfm, logfile_nl = fh.createOutputAndLogFiles(output_base_fname, log_base, "reg.xfm")
+        p.addStage(xfmconcat([linxfm, nl2], nlxfm, logfile_nl))
+        return nlxfm
+
+
+    def create_base_names(self):
+        target_base_fname = fh.removeFileExt(self.target)
+        template_base_fname = fh.removeFileExt(self.template.image)
+        output_template_dir = fh.createSubDir(abspath(self.root_dir), template_base_fname)
+        output_dir = fh.createSubDir(output_template_dir, target_base_fname)
+        log_dir = fh.createLogDir(output_dir)
+        log_base = log_dir + "/"
+        output_base_fname = output_dir + "/"
+        return output_base_fname, log_base, output_dir
+
     def build_pipeline(self):
         p = Pipeline()
         
@@ -79,61 +112,13 @@ class SMATregister:
         # <output_dir>/log/
         #
         # 
-        target_base_fname   = fh.removeFileExt(self.target)
-        template_base_fname = fh.removeFileExt(self.template.image)
-        output_template_dir = fh.createSubDir(abspath(self.root_dir), template_base_fname)
-        
-        output_dir = fh.createSubDir(output_template_dir, target_base_fname)    
-        log_dir = fh.createLogDir(output_dir)
-    
-        log_base = log_dir + "/"
-        output_base_fname = output_dir + "/"
+        output_base_fname, log_base, output_dir = self.create_base_names()
         
         inuc, inuc_log = fh.createOutputAndLogFiles(output_base_fname, log_base, "nuc.mnc" )
         p.addStage(nu_correct(self.target, inuc, inuc_log))
         
-        # linear registration to TAL space, using the template as a model      
-        linxfm, linreg_log = fh.createOutputAndLogFiles(output_base_fname, log_base, "lin.xfm" )
-        p.addStage(bestlinreg(inuc, self.template.image, linxfm, linreg_log))
-        
-        #assert template.model_dir != "", "Expected model directory supplied for mritotal linear registration step"        
-        #p.addStage(mritotal(inuc, linxfm, template.model_dir, fh.removeFileExt(template.image), linreg_log))
-        
-        if not test_mode:
-            linres, linres_log = fh.createOutputAndLogFiles(output_base_fname, log_base, "linres.mnc" )
-            p.addStage(mincresample(inuc, linres, linres_log, argarray=["-sinc", "-width", "2"], like=self.template.image, cxfm=linxfm))
-    
-            # non-linear registration        
-            iterations = 15  
-            nl0, logfile0 = fh.createOutputAndLogFiles(output_base_fname, log_base, "nl_0.xfm" )
-            nl1, logfile1 = fh.createOutputAndLogFiles(output_base_fname, log_base, "nl_1.xfm" )
-            nl2, logfile2 = fh.createOutputAndLogFiles(output_base_fname, log_base, "nl_2.xfm" )
-        
-
-            p.addStage(minctracc(linres, self.template.roi, nl0, logfile0, 
-                                step=4,
-                                sub_lattice=8, 
-                                iterations=iterations, 
-                                ident=True))
-            
-        
-            p.addStage(minctracc(linres, self.template.roi, nl1, logfile1, 
-                                 step=2,
-                                 sub_lattice=8, 
-                                 transform = nl0,
-                                 iterations=iterations,
-                                 ident=True))
-            p.addStage(minctracc(linres, self.template.roi, nl2, logfile2, 
-                                 step=1,
-                                 sub_lattice=6, 
-                                 transform = nl1,
-                                 iterations=iterations))
-        
-            nlxfm, logfile_nl = fh.createOutputAndLogFiles(output_base_fname, log_base, "reg.xfm" )
-            p.addStage(xfmconcat([linxfm, nl2], nlxfm, logfile_nl))
-            
-        else:
-            nlxfm = linxfm
+        linxfm = self.do_linear_registration(p, log_base, output_base_fname, inuc)
+        nlxfm = self.do_non_linear_registration(p, log_base, output_base_fname, inuc, linxfm)
             
         # resample labels with final registration
         resargs = ["-nearest_neighbour", "-invert", "-byte"]
@@ -146,7 +131,51 @@ class SMATregister:
         output_template = Template(self.target, labels, roi=self.target, directory = output_dir)
         
         return (p, output_template)
+
+        
+class BasicMAGeT():
+    def __init__(self, atlases, subject_images):
+        self.atlases = atlases
+        self.subject_images = subject_images
+        self.max_templates = len(subject_images)
+        
+    def set_label_propagation_method(self, method):
+        """Sets the label propagation method to use.
+        
+           Method should be a SMATRegistration or subclass
+        """
+        pass
     
+    def set_max_templates(self, max_templates):
+        self.max_templates = max_templates
+        
+    def get_templates(self):
+        return self.subject_images[:min(len(self.subject_images), self.max_templates)]
+        
+    def build_pipeline(self, pipeline, validation_labels_dir, registrations_dir, labels_dir):
+        templates = []
+        
+        # for each atlas, register to all of the templates in order to build the template library
+        for atlas in self.atlases: 
+            for subject_image in self.get_templates():
+                sp = BasicLabelPropogationStrategy(subject_image, atlas, root_dir=registrations_dir)
+                pipeline, output_template = sp.build_pipeline()
+                templates.append(output_template)
+                p.addPipeline(pipeline)
+
+        # once the initial templates have been created, go and register each
+        # subject to the templates
+        for subject_image in self.subject_images:
+            labels = []
+            for t in templates:
+                root_dir = os.path.dirname(t.directory) 
+                sp = BasicLabelPropogationStrategy(subject_image, t, root_dir=root_dir)
+                pipeline, output_template = sp.build_pipeline()
+                labels.append(InputFile(output_template.labels))
+                p.addPipeline(pipeline)
+            
+            majority_vote(subject_image, labels, p, labels_dir, validation_labels_dir)
+
 def get_labels_for_image(image_file, labels_dir):
     """Get the labels file for the given image.
     
@@ -154,8 +183,25 @@ def get_labels_for_image(image_file, labels_dir):
     """
     return os.path.join(labels_dir, fh.removeFileExt(image_file) + "_labels.mnc")
 
-def volume_similarity(labels, validation_labels, output_dir, pipeline):
-    pass
+def xcorr_vote(subject_files, registration_dir, output_dir):
+    #
+    # for each subject, calculate the cross-correlation between it and each of the templates.
+    #  
+    # cross-correlation is calculated on a mask created from the dialated average of all the labels
+    # across all of the templates for the subject
+    #
+    # Output for this step is in the following form:
+    #  <output_dir>/<subject>/xcorr_mask.mnc
+    #  <output_dir>/<subject>/templates.txt      # a list of templates used
+    #  <registration_dir>/<atlas>/<template>/<subject>/xcorr.txt
+    #  <output_dir>/<subject>/labels.mnc    
+    
+    # STEP 1: calculate the correlation masks
+    for subject_file in subject_files:
+        subject_name = fh.removeFileExt(subject_file)
+        for maget_subjects_dir in glob.glob(registration_dir + "/*/*/" + subject_name):
+            pass
+
 
 def majority_vote(subject, labels, pipeline, output_dir, validation_labels_dir = None):
     subject_base_fname = fh.removeFileExt(subject)
@@ -183,22 +229,8 @@ def majority_vote(subject, labels, pipeline, output_dir, validation_labels_dir =
     validate = CmdStage(["volume_similarity.sh", InputFile(voted_labels), subject_validation_labels, OutputFile(validation_output_file)])
     validate.setLogFile(LogFile(log))
     pipeline.addStage(validate)
-        
-class BasicMAGeT():
-    def __init__(self, atlases, subjects, validation_labels_dir):
-        pass
-    def set_label_propagation_method(self, method):
-        """Sets the label propagation method to use.
-        
-           Method should be a SMATRegistration or subclass
-        """
-        pass
-    
-    def build_pipeline(self, output_dir, pipeline):
-        """Builds a MAGeT pipeline."""
-        pass
 
-        
+    
 if __name__ == "__main__":
     usage = "%prog [options] subjects_dir"
     description = "subjects_dir holds the subject brain images in .mnc format"
@@ -289,42 +321,19 @@ if __name__ == "__main__":
             p.restart()
         else:
             subjects_dir = args[0]
+            atlas_images_dir = options.atlas_images
+            atlas_labels_dir = options.atlas_labels
+            validation_labels_dir = options.validation_labels
             
-            # get a list of the subjects and atlases
-            atlas_images  = glob.glob(os.path.join(options.atlas_images, "*.mnc"))
+            atlases = []
+            for atlas_image in glob.glob(os.path.join(atlas_images_dir, "*.mnc")):
+                atlases.append(Template(atlas_image, get_labels_for_image(atlas_image, atlas_labels_dir)))
+            
             subject_files = glob.glob(os.path.join(subjects_dir,"*.mnc"))
             
-            # create the initial templates - either total number of files
-            # or the maximum number of templates, whichever is lesser
-            templates = []
-            numTemplates = min(len(subject_files), options.max_templates)
-            
-    
-            # for each atlas, register to all of the templates in order to build the template library
-            for atlas_image in atlas_images: 
-                atlas_labels = get_labels_for_image(atlas_image, options.atlas_labels) 
-                atlas_template = Template(atlas_image, atlas_labels)
-                
-                for nfile in range(numTemplates):
-                    sp = SMATregister(subject_files[nfile], atlas_template, root_dir=registrations_dir)
-                    pipeline, output_template = sp.build_pipeline()
-                    templates.append(output_template)
-                    p.addPipeline(pipeline)
-
-            # once the initial templates have been created, go and register each
-            # subject to the templates
-            for subject in subject_files:
-                labels = []
-                for t in templates:
-                    root_dir = os.path.dirname(t.directory) 
-                    sp = SMATregister(subject, t, root_dir=root_dir)
-                    pipeline, output_template = sp.build_pipeline()
-                    labels.append(InputFile(output_template.labels))
-                    p.addPipeline(pipeline)
-                
-                majority_vote(subject, labels, p, labels_dir, options.validation_labels)
-                
-
+            maget = BasicMAGeT(atlases, subject_files)
+            maget.set_max_templates(options.max_templates)
+            maget.build_pipeline(p, validation_labels_dir, registrations_dir, labels_dir)
                     
             p.initialize()
             p.printStages()
@@ -338,6 +347,5 @@ if __name__ == "__main__":
         returnEvent = Event()
         pipelineDaemon(p, returnEvent, options, sys.argv[0])
         returnEvent.wait()
-        print "templates: " + str(numTemplates)
 
     
