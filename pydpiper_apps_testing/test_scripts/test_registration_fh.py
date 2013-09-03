a30e3434ec3aa7ca4ff07cfff333e233bc11c9f0
Miriam Friedel <mfriedel@phenogenomics.ca>
2013-09-03 16:01:35 -0400

    Updated directory names and structure based on conversation with Matthijs. There are now two test directories: pydpiper_testing (for framework py_test scripts) and pydpiper_apps_testing (for testing bits of application code)
    Right now pydpiper_apps_testing has one subdirectory (test_scripts) and will have a second directory (test_data) once data is added.
    (Git does not like to let you add empty directories to a repository.)

---
+++ pydpiper_apps_testing/test_scripts/test_registration_fh.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+from minctracc import *
+
+#Tests
+
+# RegistrationPipeFH initialization
+# Check that appropriate directories are formed. basename, basedir etc
+
+#newGroup
+# test adding twice
+# multiple tests with inputVolume and groupName specified and not specified
+
+# log from File
+
+#RegPipeFH - all getters and setters
+
+# isFileHandler method
+
+#RegistrationGroupedFiles
+# 1. Test initialization function
+# 2. getBlur (both with and without fwhm and gradient specified)
+# 3. addblur (both with and without gradient)
+
+
