4845d0231b74872d0fc3d713376f1a6eaab0331d
Ben Darwin <bcdarwin@gmail.com>
2016-01-19 19:04:37 -0500

    add CCM config file to data_files

--- setup.py
+++ setup.py
@@ -20,6 +20,6 @@ setup(name='pydpiper',
         'pydot'
       ],
       packages=['pydpiper', 'applications', 'atoms_and_modules'], 
-      data_files=[('config', ['config/MICe.cfg','config/MICe_dev.cfg','config/SciNet.cfg','config/SciNet_debug.cfg'])],
+      data_files=[('config', ['config/CCM_HPF.cfg', 'config/MICe.cfg','config/MICe_dev.cfg','config/SciNet.cfg','config/SciNet_debug.cfg'])],
       scripts=['pydpiper/pipeline_executor.py', 'pydpiper/check_pipeline_status.py', 'applications/MAGeT.py', 'applications/MBM.py', 'applications/registration_chain.py',
                'applications/twolevel_model_building.py', 'applications/pairwise_nlin.py', 'atoms_and_modules/NLIN.py', 'atoms_and_modules/LSQ12.py', 'atoms_and_modules/LSQ6.py'])
