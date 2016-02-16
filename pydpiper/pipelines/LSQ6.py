2c735f2f7323c41f7c1f5e7a2bc1ec6de8cfd9b8
Ben Darwin <bcdarwin@gmail.com>
2016-02-16 16:40:43 -0500

    add LSQ6 pipeline; lots of code duplication ...

--- pydpiper/pipelines/LSQ6.py
+++ pydpiper/pipelines/LSQ6.py
@@ -1,60 +1,73 @@
 #!/usr/bin/env python3
 #TODO s/MincAtom/MincFile/g
 
-from pydpiper.core.stages       import CmdStage, Result, Stages
-from pydpiper.core.util         import raise_
-from pydpiper.minc.files        import MincAtom
-from pydpiper.minc.registration import multilevel_minctracc, nu_correct, inormalize
+import os
 
-def lsq6_pipeline_part(options):
-    """Run a pipeline from its command-line inputs"""
-    # TODO shouldn't options.initial_model already be a MincFile?
-    # if we pass it in to several parts of the pipeline (LSQ6, LSQ12, etc.) it will be re-initialized ...
-    # FIXME account in a nice/uniform way for possibility of transform supplied with initial model/lsq6 target
+from pydpiper.core.arguments import lsq6_parser
+from pydpiper.core.stages import Stages, Result
+from pydpiper.execution.application import mk_application
+from pydpiper.minc.files import MincAtom
+from pydpiper.minc.registration import get_resolution_from_file, registration_targets, lsq6_nuc_inorm
 
-    lsq6_target = MincAtom(options.initial_model if options.initial_model else
-                           options.lsq6_target   if options.lsq6_target   else
-                           imgs[0]               if options.bootstrap     else
-                           raise_(ValueError("No target for LSQ6 registration supplied (via options.{initial_model,lsq6_target,bootstrap})")))
-    # switch(options.target_specification,
-    #  { 'bootstrap'   : imgs[0],
-    #    'lsq6-target' : options.lsq6_target,   # TODO use a mutually exclusive group for these options
-    #    'init-model'  : options.initial_model })
-    # NotImplemented # initial model or imgs[0] or chosen image ...
-    # TODO make a new file here based on lsq6_target but which can have its own writeable directory ...
-    raise NotImplemented
-   
-
-def lsq6(imgs, target, options): # [mnc], mnc, LSQ6Conf -> Result(..., output=???)
+def generic_pipeline(options):
     s = Stages()
 
-    # TODO verify that options (lsq6_{method,target,...}) make sense ... or should this be done before lsq6 call?
-    lsq6_method = options.lsq6_method or 'lsq6_large_rotations'
+    output_dir    = options.application.output_directory   # can't be used outside here ...
+    pipeline_name = options.application.pipeline_name
 
-    def switch(k, choices): # A, { A : () -> B } -> B
-        # TODO move to util module, add optional default, add wildcards, turn into preprocessor ...
-        try:
-            v = choices[k]
-        except KeyError:
-            raise SwitchError('unhandled case %s (no default supplied); valid: %s' % (k, list(choices.keys())))
-        else:
-            return v()
+    if len(options.application.files) == 0:
+        raise ValueError("Please, some files!")
 
-    # I don't like the fact that the whole LSQ6 module seems to be a switch around the lsq6 registration ... or maybe I do ...
-    lsq6_xfms = s.defer(switch(lsq6_method, # TODO do we need an `initial_model` field or does `target` suffice? registration should be agnostic to source of target, right?
-      { 'lsq6_simple'            : lambda :  # TODO the options args shouldn't be string-indexed dictionaries but conf objects ...
-          lsq6_hierarchical_minctracc(sources=imgs, target=lsq6_target, options={ 'initial_transform' : 'identity', 'lsq6_protocol' : NotImplemented }),
-        'lsq6_centre_estimation' : lambda :
-          lsq6_hierarchical_minctracc(sources=imgs, target=lsq6_target, options={}),
-        'lsq6_large_rotations'   : lambda :
-          lsq6_rotational_minctracc(sources=imgs,   target=lsq6_target, options={})
-      }))
+    # TODO this is quite tedious and duplicates stuff in the registration chain ...
+    resolution = (options.registration.resolution or
+                  get_resolution_from_file(
+                      registration_targets(lsq6_conf=options.lsq6,  # not really generic due to use of options.lsq6 ...
+                                           app_conf=options.application).registration_standard.path))
+    options.registration = options.registration.replace(resolution=resolution)
 
-    return Result(stages=s, output=lsq6_xfms)
+    targets = registration_targets(lsq6_conf=options.lsq6,  # see above ...
+                                   app_conf=options.application,
+                                   first_input_file=options.application.files[0])
 
-# TODO make higher-order way to run part of a pipeline on certain images, then create new xfmhs with replaced inputs/resampled files
-# (idea: images have same 'geometry'/coordinates but may be modified, e.g., tagged kidney tip images)
-# may need to supply some fns to determine how to get/set outputs as our interfaces aren't uniform enough to know this in general
+    return (options, targets)
+
+def lsq6_pipeline(options):
+    # TODO could also allow pluggable pipeline parts e.g. LSQ6 could be substituted out for the modified LSQ6
+    # for the kidney tips, etc...
+    output_dir    = options.application.output_directory
+    pipeline_name = options.application.pipeline_name
+
+    # TODO this is tedious and annoyingly similar to the registration chain and MBM ...
+    processed_dir = os.path.join(output_dir, pipeline_name + "_processed")
+
+    imgs = [MincAtom(name, pipeline_sub_dir=processed_dir) for name in options.application.files]
+
+    s = Stages()
+
+    if len(options.application.files) == 0:
+        raise ValueError("Please, some files!")
+
+    # TODO this is quite tedious and duplicates stuff in the registration chain ...
+    resolution = (options.registration.resolution or
+                  get_resolution_from_file(
+                      registration_targets(lsq6_conf=options.lsq6,
+                                           app_conf=options.application).registration_standard.path))
+    options.registration = options.registration.replace(resolution=resolution)
+
+    # FIXME: why do we have to call registration_targets *outside* of lsq6_nuc_inorm? is it just because of the extra
+    # options required?
+    targets = registration_targets(lsq6_conf=options.lsq6,
+                                   app_conf=options.application,
+                                   first_input_file=options.application.files[0])
+
+    lsq6_result = s.defer(lsq6_nuc_inorm(imgs=imgs,
+                                         resolution=resolution,
+                                         registration_targets=targets,
+                                         lsq6_options=options.lsq6))
+
+    return Result(stages=s, output=lsq6_result)
+
+lsq6_application = mk_application(parsers=[lsq6_parser], pipeline=lsq6_pipeline)
 
 if __name__ == "__main__":
-    pass
+    lsq6_application()
