f22883ba388d3875f09e8ebb8f54c3dcaf0d7398
Ben Darwin <bcdarwin@gmail.com>
2016-03-17 11:59:21 -0400

    lots of nasty boilerplate for the lsq12 pipeline

--- pydpiper/pipelines/LSQ12.py
+++ pydpiper/pipelines/LSQ12.py
@@ -14,14 +14,22 @@ from pydpiper.minc.files import MincAtom
 # Q. should a pipeline (post-cmdline-parsing) take a `files` arg
 # (an array of minc files) or just an options arg containing filenames?
 def LSQ12_pipeline(options):
-    imgs = [MincAtom(f) for f in options.application.files]
+
+    output_dir    = options.application.output_directory
+    pipeline_name = options.application.pipeline_name
+
+    # TODO this is tedious and annoyingly similar to the registration chain and MBM and LSQ6 ...
+    processed_dir = os.path.join(output_dir, pipeline_name + "_processed")
+    lsq12_dir     = os.path.join(output_dir, pipeline_name + "_lsq12")
+
+    imgs = [MincAtom(f, pipeline_sub_dir=processed_dir) for f in options.application.files]
 
     # determine LSQ12 settings by overriding defaults with
     # any settings present in protocol file, if it exists
     # could add a hook to print a message announcing completion, output files,
     # add more stages here to make a CSV
-    return lsq12_pairwise(imgs, lsq12_conf=options.lsq12,
-                          lsq12_dir=options.application.output_directory or os.getcwd())
+
+    return lsq12_pairwise(imgs, lsq12_conf=options.lsq12, lsq12_dir=lsq12_dir)
 
 def main(args):
     # this is probably too much heavy machinery since the options aren't tree-shaped; could just use previous style
