62a54118d30fc97872690ecb8c89fa88fb1be8d6
Ben Darwin <bcdarwin@gmail.com>
2016-08-02 11:26:46 -0400

    provide dependency link for ConfigArgParse until the maintainer makes a new release

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='pydpiper',
-      version='1.18',
+      version='1.18.1',
       license='Modified BSD',
       description='Python code for flexible pipeline control',
       long_description = 'Python code for flexible pipeline control', 
@@ -23,4 +23,5 @@ setup(name='pydpiper',
       packages=['pydpiper', 'applications', 'atoms_and_modules'], 
       data_files=[('config', ['config/CCM_HPF.cfg', 'config/MICe.cfg','config/MICe_dev.cfg','config/SciNet.cfg','config/SciNet_debug.cfg'])],
       scripts=['pydpiper/pipeline_executor.py', 'pydpiper/check_pipeline_status.py', 'applications/MAGeT.py', 'applications/MBM.py', 'applications/registration_chain.py',
-               'applications/twolevel_model_building.py', 'applications/pairwise_nlin.py', 'atoms_and_modules/NLIN.py', 'atoms_and_modules/LSQ12.py', 'atoms_and_modules/LSQ6.py'])
+               'applications/twolevel_model_building.py', 'applications/pairwise_nlin.py', 'atoms_and_modules/NLIN.py', 'atoms_and_modules/LSQ12.py', 'atoms_and_modules/LSQ6.py'],
+      dependency_links=["https://github.com/bw2/ConfigArgParse/archive/574f231d05d7d15c6f3eea08b783ffae858add3f.tar.gz#egg=ConfigArgParse-0.11.0"])
