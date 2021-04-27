15c4e5bb482502a73f43d3e501c5ee7c72f6f8fc
Ben Darwin <bcdarwin@gmail.com>
2021-04-27 10:59:03 -0400

    typo

--- pydpiper/execution/pipeline_executor.py
+++ pydpiper/execution/pipeline_executor.py
@@ -634,7 +634,7 @@ def main():
     default_config_file = os.getenv("PYDPIPER_CONFIG_FILE")
     if default_config_file is not None:
         try:
-            with open(PYDPIPER_CONFIG_FILE):
+            with open(default_config_file):
                 pass
         except:
             warnings.warn(f"PYDPIPER_CONFIG_FILE is set to '{default_config_file}', which can't be opened.")
